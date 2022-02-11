efb265e5612ff32ab24bd7d2a055dd0727499fcc
morbi <elae.debichi@esprit.tn>
2022-02-11 12:09:24 +0100

    models + admin

---
+++ App/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin,messages
+
+# Register your models here.
+from App.models import Etudiant, Coach, Projet
+
+admin.site.register(Etudiant)
+admin.site.register(Coach)
+# admin.site.register(Projet, ProjetAdmin)
+
+
+@admin.register(Projet)
+class ProjetAdmin(admin.ModelAdmin):
+    list_display = (
+    'nom_projet', 'superviseur', 'duree_projet', 'temps_alloue_par_projet', 'besoins', 'est_valide', 'createur',)
+    fieldsets = (('A propos', {'fields': ('nom_projet', 'besoins', 'description',)}),
+                 ('Etat', {'fields': ('est_valide',)}),
+                 ('Duree', {'fields': ('duree_projet', 'temps_alloue_par_projet',)}),
+                 ('People', {'fields': ('superviseur', 'createur',)}))
+    list_per_page = 2
+
+    def set_to_valid(self,request, queryset):
+        queryset.update(est_valide=True)
+
+    def set_to_invalid(self,request, queryset):
+        row_invalid = queryset.filter(est_valide=False)
+        if(row_invalid.count()>0)
+            messages.errors(request,"%s this article is already invalid"%row_invalid.count())
+        #queryset.update(est_valide=False)
+
+
+    set_to_valid.short_description= 'validate project'
+    set_to_invalid.short_description= 'invalidate project'
+    actions = ['set_to_valid','set_to_invalid']
+
+
+
+
+
---
+++ App/models.py
@@ -0,0 +1,65 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+
+
+def is_esprit_email(value):
+    if not str(value).endswith('@esprit.tn'):
+        raise ValidationError("your email is invalid. ", params={'value': value})
+
+# Create your models here.
+
+
+class User(models.Model):
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    email = models.EmailField('Email', validators=[is_esprit_email])
+    def __str__(self):
+        #return 'le prenom: '+self.prenom+', le nom: '+self.nom
+        return f'le prenom {self.prenom} le nom {self.nom}'
+
+class Etudiant(User):
+    groupe = models.CharField(max_length=30)
+
+
+class Coach(User):
+    pass
+
+
+class Projet(models.Model):
+    nom_projet = models.CharField('Titre Projet', max_length=30)
+    duree_projet = models.IntegerField('Duree Estimee', default=0)
+    temps_alloue_par_projet = models.IntegerField('Temps Alloue', validators=[MinValueValidator(1), MaxValueValidator(10)])
+    besoins = models.TextField(max_length=100)
+    description = models.TextField(max_length=100)
+    est_valide = models.BooleanField(default=False)
+    createur = models.OneToOneField(
+        Etudiant,
+        related_name='project_owner',
+        on_delete=models.CASCADE
+
+    )
+    superviseur = models.ForeignKey(
+        Coach,
+        on_delete= models.SET_NULL,
+        null= True,
+        related_name= 'project_coach',
+
+    )
+    members = models.ManyToManyField(
+        Etudiant,
+        through= 'MembershipInProject',
+        related_name= 'les_membres',
+
+    )
+
+
+class MembershipInProject(models.Model):
+        projet= models.ForeignKey(Projet, on_delete=models.CASCADE)
+        etudiant= models.ForeignKey(Etudiant, on_delete=models.CASCADE)
+        time_allocated_by_member= models.IntegerField('temps alloué par les membres')
+
+
+
